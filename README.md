66b1cf6fc9179139bbd1973b4550a0b109457689
Dan Ford <dpford@gmail.com>
2014-07-30 11:51:21 -0400

    Makes the full request, updated readme

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ flask-govdelivery
 =================
 
 A flask wrapper for the govdelivery API
+
+It currently provides a form handler that allows for subscription to an arbitrary number of topics. Forms that submit to it should look roughly like this:
+
+```html
+<form action="/subscriptions/new" method="POST" enctype="application/x-www-form-urlencoded">
+
+<input type="hidden" name="code" value="SOME_TOPIC_CODE">
+<input type="hidden" name="code" value="ANOTHER_TOPIC_CODE">
+<input type="input" name="questionid_12345"/>
+<input type="email" name="email" >
+<button>Sign up</button>
+</form>
+```
+The following environment variables need to be set:
+
+* GOVDELIVERY_BASE_URL
+* GOVDELIVERY_ACCOUNT_CODE
+* GOVDELIVERY_USER
+* GOVDELIVERY_PASSWORD
+
+Depends on the govdelivery python wrapper being installed:
+https://github.com/rosskarchner/govdelivery
--- app/flask_govdelivery/controllers.py
+++ app/flask_govdelivery/controllers.py
@@ -1,16 +1,40 @@
-from flask import Blueprint, request, Response
+import os
+from flask import Blueprint, request, Response, render_template
+from govdelivery.api import GovDelivery
+
+ACCOUNT_CODE = os.environ.get('GOVDELIVERY_ACCOUNT_CODE')
+gd = GovDelivery(account_code=ACCOUNT_CODE)
 
 flask_govdelivery = Blueprint("flask-govdelivery", __name__, url_prefix="")
 
 
-@flask_govdelivery.errorhandler(400)
 def missing_parameter(parameter):
-    message = "You are missing the {} parameter".format(parameter)
+    message = 'You are missing the "{}" parameter'.format(parameter)
     return Response(message, 400)
+
+def fail_with_code_and_message(message, code=500):
+    return Response(message, code)
+
+def extract_answers_from_request(request):
+    answers = [(param.split('_')[1], value) for param, value in \
+               request.form.items() if param.startswith('questionid')]
+    return answers
 
 
 @flask_govdelivery.route("/new", methods=['POST'])
-def hello():
-    if 'test' not in request.form:
-        return missing_parameter('test')
-    return "works"
+def new():
+    for required_param in ['email','code']:
+            if required_param not in request.form:
+                return missing_parameter(required_param)
+    email_address = request.form['email']
+    codes = request.form.getlist('code')
+    try:
+        subscription_response = gd.set_subscriber_topics(email_address, codes)
+        if subscription_response.status_code != 200:
+            return fail_with_code_and_message(500, "subscription attempt failed")
+    except Exception, e:
+        return fail_with_code_and_message(500, "subscription attempt failed")
+    answers = extract_answers_from_request(request)
+    for question_id, answer_text in answers:
+            response = gd.set_subscriber_answers_to_question(email_address, question_id, answer_text)
+    return Response("subscription successful!")
